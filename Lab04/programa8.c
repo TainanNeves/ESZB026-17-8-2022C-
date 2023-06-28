53565f84b636f3eb6e4bd657806c5454218b8889
ma-x-om <126175949+ma-x-om@users.noreply.github.com>
2023-06-28 13:11:20 -0300

    Update programa8.c

--- Lab04/programa8.c
+++ Lab04/programa8.c
@@ -15,22 +15,32 @@ int main(){
    wiringPiSetupGpio();    // usar a numeracao GPIO , nao WPi
 
    //Configurando a comunicação
-   int valor;
-   if((valor = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY ))<0){
+   int file;
+   if((file = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY ))<0){
       perror("UART: Falha ao abrir o arquivo.\n");
       return -1;
    }
    struct termios options;
-   tcgetattr(valor, &options);
+   tcgetattr(file, &options);
    // 115200 baud , 8-bit , enable receiver , no modem control lines
    options.c_cflag = B115200 | CS8 | CREAD | CLOCAL;
    options.c_iflag = IGNPAR | ICRNL ; // ignora erros de paridade
-   tcflush(valor , TCIFLUSH); // descarta informacao no arquivo
-   tcsetattr(file, TCSANOW, & options); // aplica alteracoes imediatamente
-   unsigned char transmit [23] = " Testando a comunicacao \0"; //Teste de counicação
-
+   tcflush(file , TCIFLUSH); // descarta informacao no arquivo
+   tcsetattr(file, TCSANOW, &options); // aplica alteracoes imediatamente
+   //unsigned char transmit [23] = " Testando a comunicacao \0"; //Teste de counicação
+   //   |-> nesse caso a comunicação é testada nas linhas seguintes
+   int range = 100;
+   unsigned char receive [100];
+   if (( count = read ( file , ( void *) receive , 100) ) <0) { // recebe os dados
+      perror ( " Falha ao ler da entrada \ n " ) ;
+      return -1;
+   }
+   if ( count ==0) printf ( " Nao houve resposta !\ n " );
+   // adicionar ELSE igual a Listing 5: lab04/comando.c
    softPwmCreate(pino_PWM, 1, range); //Inicia o PWM por software
+   delay (1000) // aguarda 1 s para sabermos que ligou e está prestes a entrar no loop seguinte
 
+   valor = range*receive//////////////// arrumar a partir aqui
    while (1) {                         //Roda infinitamente
       brilho = valor;
       softPwmWrite(pino_PWM, brilho);
