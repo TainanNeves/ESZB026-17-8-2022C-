d6258bc4a507dacea347fc199166b9be93a40c1f
TainanEMateus <tainancneves@gmail.com>
2023-07-24 20:53:35 -0300

    Atualizacao durante a aula

--- Lab07/gnuplot/grafico.sh
+++ Lab07/gnuplot/grafico.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-ARQUIVODADOS=/home/pi/sist_embarcados_git/lab07/gnuplot/dados.txt
-ARQUIVOSAIDA=/home/pi/sist_embarcados_git/lab07/gnuplot/dados.png
+ARQUIVODADOS=/home/pi/ESZB026-17-8-2022C-/Lab07/gnuplot/dados_2.txt
+ARQUIVOSAIDA=/home/pi/ESZB026-17-8-2022C-/Lab07/gnuplot/dados_2.png
 
 gnuplot << EOF
-set title "Título"
+set title "Grafico 2"
 set ylabel "Eixo Y"
 set xlabel "Eixo X"
 set terminal png
 set output "$ARQUIVOSAIDA"
 plot "$ARQUIVODADOS" \
-     linecolor rgb '#0060ad' \
+     linecolor rgb '#FFFF00' \
      linetype 1 \
      linewidth 1 \
      pointtype 2 \
