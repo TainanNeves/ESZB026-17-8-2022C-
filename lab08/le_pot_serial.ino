b878f204a2a47530ae34bab38b27a242480b3032
TainanNeves <71852778+TainanNeves@users.noreply.github.com>
2023-07-31 20:59:08 -0300

    Add files via upload

---
+++ lab08/le_pot_serial.ino
@@ -0,0 +1,46 @@
+const int analogInPin = A0;            // o potenciômetro esta ligado ao pino A0
+int iniciaColeta = 0;
+char charRecebido;                     // cria uma variavel para armazenar o caractere recebido
+int nosso_delay = 100;
+
+void setup(){
+   // Configura a serial: baud rate de 115200, 8-bit, sem paridade, 1 stop bit
+   Serial.begin(115200, SERIAL_8N1);
+   
+}
+
+void loop(){
+   if (Serial.available()){            // verifica se recebeu algum comando
+      charRecebido = Serial.read();    // le o caractere recebido
+      switch ( charRecebido ){
+          case 'i':                    // inicia coleta
+             iniciaColeta = 1;
+             break;
+             
+          case 'p':                    // para a coleta
+             iniciaColeta = 0;
+             brea k;
+          case 'a':                    // para a coleta
+             nosso_delay += 10;
+             Serial.println(nosso_delay);
+             break;
+          case 'd':                    // para a coleta
+             if (nosso_delay > 10){
+             nosso_delay -= 10;
+             Serial.println(nosso_delay);}
+             break;
+          case 't':
+              Serial.println(nosso_delay);
+              break;
+             
+          default:                     // outro comando, ignora...
+             break;
+      }
+   }
+   if ( iniciaColeta == 1 ){
+       int valor = analogRead(analogInPin); // le valor no pino A0 usando conversor ADC de 10-bit
+       Serial.write(valor & 0xFF);          // envia byte menos significativo
+       Serial.write(valor >> 8);            // envia byte mais significativo
+   }
+   delay(nosso_delay);                          // aguarda 100ms
+}
