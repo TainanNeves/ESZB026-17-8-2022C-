07eb4cc9312983edb386cb7b7aab2f0742e972c8
ma-x-om <mateus.ramirez@outlook.com>
2023-08-14 20:44:33 -0300

    Files for website

--- Projeto/spi/fc_grafico_spi.sh
+++ Projeto/spi/fc_grafico_spi.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 ARQUIVODADOS=/home/pi/fc_medidas.txt
+CSVDADOS=/home/pi/dados_gravados_10ss.csv
 ARQUIVOSAIDA=/home/pi/fc_medidas.png
 
 gnuplot << EOF
+
 set title "Análise da Freq. Cardiaca"
+set datafile separator ','
 set ylabel "Freq. Cardiaca [bpm]"
 set xlabel "Tempo [h]"
 set terminal png
 set output "$ARQUIVOSAIDA"
-plot "$ARQUIVODADOS" \
+plot "$CSVDADOS" \
      linecolor rgb '#0060ad' \
      linetype 1 \
      linewidth 1 \
